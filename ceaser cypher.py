b5f3f8e5efc33c24c2426ffc36e290071ba8b6fa
Arun-kh-kannery <arunkh65328@gmail.com>
2024-01-16 18:51:19 +0530

    first commit

---
+++ ceaser cypher.py
@@ -0,0 +1,33 @@
+def encryption(text,shift_key):
+    ans=""
+    for i in text:
+        if(ord('a')<= ord(i) <=ord('z')):
+            ans+=chr(ord('a')+(ord(i)-ord('a')+shift_key)%26)
+        elif(ord('A') <= ord(i) <=ord('Z')):
+            ans+=chr(ord('A')+(ord(i)-ord('A')+shift_key)%26)
+        else:
+            ans+=i
+    print(f"Here is the text after encryption : {ans}")
+def decryption(text,shift_key):
+    ans=""
+    for i in text:
+        if (ord('a') <= ord(i) <= ord('z')):
+            ans += chr(ord('a') + (ord(i) - ord('a') - shift_key) % 26)
+        elif (ord('A') <= ord(i) <= ord('Z')):
+            ans += chr(ord('A') + (ord(i) - ord('A') - shift_key) % 26)
+        else:
+            ans += i
+    print(f"Here is the text after decryption: {ans}")
+while(True):
+    operation=input("Type 'encrypt' for encryption,Type 'decrypt' for decryption :\n")
+    text=input("Enter your message:\n ")
+    shift_key=int(input("Enter Shift_Key:\n"))
+    if(operation == 'encrypt'):
+        encryption(text,shift_key)
+    elif(operation=="decrypt"):
+        decryption(text,shift_key)
+    play_again=input("Type 'yes' to continue,Type 'no' to exit:\n")
+    if(play_again=="no"):
+        print('Have a nice day ')
+        exit()
+
